4fac6300ad386c41cb0d4f4c4fd91a1db041b800
GNITHINKUMAR <gnithinkumar1306@gmail.com>
2024-07-18 20:07:34 +0530

    New Traversy Project created API

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,22 +1,23 @@
-const getGoals = (req,res)=>{
+const asyncHandler = require('express-async-Handler');
+const getGoals = asyncHandler(async(req,res)=>{
     res.status(200).json({message :' get goals'})
-}
+})
 
-const setGoals = (req,res)=>{
+const setGoals = asyncHandler(async(req,res)=>{
     if(!req.body.text){
         res.status(400)
         throw new Error('Please add a text field') 
     }
     res.status(200).json({message :' set goals'})
-}
+})
 
-const updateGoal = (req,res)=>{
+const updateGoal =asyncHandler(async(req,res)=>{
     res.status(200).json({message :` update goal ${req.params.id}`})
 }
-
-const deleteGoal = (req,res)=>{
+)
+const deleteGoal = asyncHandler(async(req,res)=>{
     res.status(200).json({message :` delete goal ${req.params.id}`})
-}
+})
 module.exports ={
     getGoals,setGoals,updateGoal,deleteGoal
 }
